6adc18c0af376b3484e0e52e282852f46f59b99a
Govnor Payne <govnorpayne@gmail.com>
2022-12-17 20:10:04 -0600

    making and finding data

--- src/components/CompareResults/CompareResultsView.tsx
+++ src/components/CompareResults/CompareResultsView.tsx
@@ -6,7 +6,9 @@ import { connect } from 'react-redux';
 import { useLocation } from 'react-router-dom';
 
 import type { RootState } from '../../common/store';
+import { useAppDispatch } from '../../hooks/app';
 import useFetchCompareResults from '../../hooks/useFetchCompareResults';
+import { fetchRevisionByID } from '../../thunks/searchThunk';
 import { Repository, Revision } from '../../types/state';
 import PerfCompareHeader from '../Shared/PerfCompareHeader';
 import SelectedRevisionsTable from '../Shared/SelectedRevisionsTable';
@@ -14,6 +16,7 @@ import CompareResultsTable from './CompareResultsTable';
 
 function CompareResultsView(props: CompareResultsViewProps) {
   const { revisions, mode } = props;
+  const dispatch = useAppDispatch();
 
   const location = useLocation();
   const { dispatchFetchCompareResults } = useFetchCompareResults();
@@ -27,12 +30,43 @@ function CompareResultsView(props: CompareResultsViewProps) {
     void dispatchFetchCompareResults(repos as Repository['name'][], revs);
   });
 
+  useEffect(() => {
+    const getRevId = async () => {
+      const searchParams = new URLSearchParams(location.search);
+      const repos = searchParams.get('repos')?.split(',') ?? [];
+      const revs = searchParams.get('revs')?.split(',') ?? [];
+
+      for (let i = 0; i < revs?.length; i++) {
+        if (repos[i] !== undefined) {
+          const revIC = await dispatch(
+            fetchRevisionByID({
+              repository: repos[i] as Repository['name'],
+              search: revs[i],
+            }),
+          );
+          console.log(revIC);
+        }
+      }
+    };
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    getRevId();
+    // revs?.forEach((revision, index) => {
+    //   if (repos[index] !== undefined) {
+    //     const revIC = fetchRevisionByID({
+    //       repository: repos[index] as Repository['name'],
+    //       search: revision,
+    //     });
+    //     console.log(revIC);
+    //   }
+    // });
+  }, []);
+
   return (
     <Container maxWidth="xl">
       <PerfCompareHeader />
       <Grid container alignItems="center" justifyContent="center">
         <Grid item xs={10}>
-          {revisions.length > 0 && (
+          {revisions.length >= 0 && (
             <SelectedRevisionsTable view="compare-results" />
           )}
         </Grid>
